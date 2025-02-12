7e4b86c378b9acbf5ecfd4e8b23dae647981581d
Starinin Andrey <starinin-andrey@ya.ru>
2025-02-12 21:31:31 +0300

    Update

--- PhoneBook.Desktop/ViewModels/MainWindowViewModel.cs
+++ PhoneBook.Desktop/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,29 @@
-﻿namespace PhoneBook.Desktop.ViewModels;
+﻿using System.Collections.ObjectModel;
+using System.Reactive;
+using PhoneBook.BLL;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace PhoneBook.Desktop.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+    public ObservableCollection<PageListItem> Pages { get; } = 
+    [
+        new() {PageViewModel = new MainPageViewModel(new Service())},
+        new() {PageViewModel = new EditPageViewModel()},
+    ];
+    [Reactive] public PageListItem SelectedPage { get; set; }
+    
+    [Reactive] public bool IsPaneOpen { get; set; }
+    
+    public ReactiveCommand<Unit, bool> CommandPaneOpenClose { get; }
+
+    public MainWindowViewModel()
+    {
+        SelectedPage = Pages[0];
+        
+        IsPaneOpen = true;
+        CommandPaneOpenClose = ReactiveCommand.Create(() => IsPaneOpen = !IsPaneOpen);
+    }
 }
--- PhoneBook.Desktop/ViewModels/ViewModelBase.cs
+++ PhoneBook.Desktop/ViewModels/ViewModelBase.cs
@@ -2,6 +2,6 @@
 
 namespace PhoneBook.Desktop.ViewModels;
 
-public class ViewModelBase : ReactiveObject
+public abstract class ViewModelBase : ReactiveObject
 {
 }
